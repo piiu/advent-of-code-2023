72bdd46d4a89c7b24770bf9f70173e7c6a1895fa
Piiu Pilt <piiu.pilt@helmes.com>
2023-12-01 08:39:28 +0200

    Day 1

--- src/Days/Day01.php
+++ src/Days/Day01.php
@@ -6,7 +6,29 @@ use AdventOfCode\Common\Solver;
 
 class Day01 extends Solver
 {
+    const NUMBERS = [
+        'one' => 1,
+        'two' => 2,
+        'three' => 3,
+        'four' => 4,
+        'five' => 5,
+        'six' => 6,
+        'seven' => 7,
+        'eight' => 8,
+        'nine' => 9
+    ];
     public function solve()
     {
+        $this->part1 = $this->part2 = 0;
+        foreach (explode(PHP_EOL, $this->input) as $line) {
+            preg_match_all('/\d/', $line, $numerics);
+            $this->part1 += (int)(reset($numerics[0]).end($numerics[0]));
+
+            preg_match_all('/(?=(\d|'.implode('|', array_keys(self::NUMBERS)).'))/', $line, $numbers);
+            $numbers = array_map(function($number) {
+                return self::NUMBERS[$number] ?? $number;
+            }, $numbers[1]);
+            $this->part2 += (int)(reset($numbers).end($numbers));
+        }
     }
 }
