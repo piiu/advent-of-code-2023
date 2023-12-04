440808c1c0f97c937db9ae683f706e8d4d1b7241
Piiu Pilt <piiu.pilt@helmes.com>
2023-12-04 09:03:42 +0200

    Day 4

---
+++ src/Days/Day04.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AdventOfCode\Days;
+
+use AdventOfCode\Common\Solver;
+
+class Day04 extends Solver
+{
+    private array $cards = [];
+
+    public function solve()
+    {
+        foreach (explode(PHP_EOL, $this->input) as $line) {
+            preg_match('/Card\s*(\w*):\s*((\w*\s*)*)\|(\s*(\w*\s*)*)/', $line, $result);
+            $this->cards[$result[1]] = count(array_intersect(preg_split('/\s+/', trim($result[2])), preg_split('/\s+/', trim($result[4]))));
+        }
+
+        $this->part1 = array_sum(array_map(function($matches) {
+            return $matches === 0 ? 0 : pow(2, $matches - 1);
+        }, $this->cards));
+
+        $this->part2 = array_sum(array_map([$this, 'getCardsReceived'], array_keys($this->cards)));
+    }
+
+    private function getCardsReceived($number, $count = 0) {
+        if ($this->cards[$number] > 0) {
+            foreach (range($number + 1, $number + $this->cards[$number]) as $receivedCard) {
+                $count += $this->getCardsReceived($receivedCard);
+            }
+        }
+        return $count + 1;
+    }
+}
