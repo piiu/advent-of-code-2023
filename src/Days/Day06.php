2b833029530397b1446496d88068c3ea12f00933
Piiu Pilt <piiu.pilt@helmes.com>
2023-12-06 09:29:19 +0200

    Day 6 - Simplify

--- src/Days/Day06.php
+++ src/Days/Day06.php
@@ -21,11 +21,12 @@ class Day06 extends Solver
      * x * (t-x) = d
      * -x^2 + t -d = 0
      * x = (-t +- sqrt(t^2 - 4 * d)) / -2
+     * 0.01 modifier to avoid repeating the record
      */
     private function solveForTimeAndDistance($time, $distance): int
     {
         $sqrt = sqrt(pow($time, 2) - (4 * $distance));
-        $minHold = ceil((-$time + $sqrt) / -2 + 0.01); // 0.01 Modifier to avoid repeating the record
+        $minHold = ceil((-$time + $sqrt) / -2 + 0.01);
         $maxHold = floor((-$time - $sqrt) / -2 - 0.01);
         return abs($maxHold - $minHold) + 1;
     }
